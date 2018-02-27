9318a5f56da767090676b29fb9e4387b1859b272
clrhogan <36135712+clrhogan@users.noreply.github.com>
2018-02-26 20:19:39 -0800

    Add files via upload

---
+++ Source/Team4Game/src/chelsea/CHGameManager.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CHGameManager.h"
+
+
+void ACHGameManager::generateMap()
+{
+	// MAMap *levelMap = GetWorld().SpawnActor<MAMAP>();		// call map generation	
+}
+
+void ACHGameManager::spawnEnemies()
+{
+	// spawn enemies
+}
+
+void ACHGameManager::initiateAssets()
+{
+	// start sound and graphics
+}
